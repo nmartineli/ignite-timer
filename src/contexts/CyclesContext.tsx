24cfff8af986990b88ebea869956ba7e49faaf91
nataliamartineli <natalia.martineli@ilia.digital>
2023-06-20 18:41:36 -0300

    Refactor to add useReducer and Immer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,17 +1,14 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useReducer, useState } from 'react'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducers'
+import {
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
 
 interface CreateCycleData {
   task: string
   minutesAmount: number
-}
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
 }
 
 interface CyclesContextType {
@@ -34,9 +31,18 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  // USEREDUCER
+  // o useReducer sempre vai receber dois parâmetros: uma função e o valor inicial da variável (no caso é o valor inicial dos ciclos)
+  // a função recebe dois parâmetros: o estado em "tempo real" e a ação que o usuário está querendo realizar para atualizar o estado. pode ser por ex: add, interrupt, tudo o que altera o estado
+  // por padrão, usamos dipatch como nome da ação que dispara um funcionamento
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+    cycles: [],
+    activeCycleId: null,
+  })
+
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
@@ -45,15 +51,19 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
+    // STATE
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, finishedDate: new Date() }
+    //     } else {
+    //       return cycle
+    //     }
+    //   }),
+    // )
+
+    // REDUCER
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -65,26 +75,44 @@ export function CyclesContextProvider({
       minutesAmount: data.minutesAmount,
       startDate: new Date(),
     }
-
+    // STATE
     // sempre que uma alteração de estado depender do estado anterior, é indicado utilizar em forma de função (closures)
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
+    // setCycles((state) => [...state, newCycle])
+
+    // REDUCER
+    // quando eu chamo o dispatch, precisa enviar uma informação que distingue uma action da outra. por padrão, usamos um objeto com type e payload
+    // dispatch({
+    //   // tipo da ação
+    //   type: ActionTypes.ADD_NEW_CYCLE,
+    //   // dados do novo ciclo que está sendo criado
+    //   payload: {
+    //     newCycle,
+    //   },
+    // })
+
+    dispatch(addNewCycleAction(newCycle))
+
     setAmountSecondsPassed(0)
 
     // reset()
   }
 
   function interruptCurrentCycle() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    )
-    setActiveCycleId(null)
+    // REDUCER
+
+    dispatch(interruptCurrentCycleAction())
+
+    // STATE
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId) {
+    //       return { ...cycle, interruptedDate: new Date() }
+    //     } else {
+    //       return cycle
+    //     }
+    //   }),
+    // )
+    // setActiveCycleId(null)
   }
 
   return (
